0c3ad2810910c670c6593ec2dc6ffdc482b1bc90
daemon1024 <barun1024@gmail.com>
2020-11-30 15:45:25 +0530

    basic pastebin ready

--- mod.ts
+++ mod.ts
@@ -1,30 +1,46 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router, Status } from "https://deno.land/x/oak/mod.ts";
+
+let count = 0;
+
+interface Paste {
+  content: string;
+}
+
+const pastes = new Map<string, Paste>();
 
 const route = new Router();
 
 // Hello World!
 route.get("/", (ctx) => {
   ctx.response.body = "Hello World!";
-}).get("/:id", (ctx) => {
-  ctx.response.body = "Content for " + ctx.params.id;
+})
+
+// Store pastes in a generated id
+route.post("/", async (ctx) => {
+  if (!ctx.request.hasBody) {
+    ctx.throw(Status.BadRequest, "Bad Request");
+  }
+  const body = ctx.request.body();
+  let paste = await body.value;
+  console.log(paste);
+  if (paste) {
+    count++;
+    pastes.set(String(count), {
+      content : paste as string
+    });
+    ctx.response.status = Status.OK;
+    ctx.response.body = `Stored succesfully at http://localhost:8000/${count} `;
+    return;
+  }
+  ctx.throw(Status.BadRequest, "Bad Request");
+})
+
+// Display stored pastes for a particular id
+route.get<{ id: string }>("/:id", (ctx) => {
+  ctx.response.body = ctx.params && pastes.has(ctx.params.id) ? pastes.get(ctx.params.id)?.content : "No paste for this id" 
 });
 
 const app = new Application();
-
-// Logger
-app.use(async (ctx, next) => {
-  await next();
-  const rt = ctx.response.headers.get("X-Response-Time");
-  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
-});
-
-// Timing
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
-});
 
 app.use(route.routes());
 app.use(route.allowedMethods());
