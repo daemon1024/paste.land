ba4d9ebf0c54d7c14c9d83a2b7fa738e254c74ed
Aayush Kumar Sahu <aayushmau5@gmail.com>
2020-12-06 17:10:37 +0530

    Add random id generator

---
+++ random.ts
@@ -0,0 +1,18 @@
+let characters =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+function randomId(length?: number): string {
+  let res = "";
+  let sum = 0;
+  length = length ? length : 4;
+  while (length) {
+    let random = Math.random();
+    res += characters.charAt(random * characters.length);
+    sum += characters.charAt(random * characters.length).charCodeAt(0);
+    length--;
+  }
+  res += (sum % 97).toString();
+  return res;
+}
+
+export { randomId };
